3b8c0d15a24e96e04caa130e82af0c4f680937ec
Daniel Rodriguez <drodriguez@metrixcolombia.com>
2025-10-03 19:44:39 -0500

    chore(project): Configuración inicial del proyecto

---
+++ src/main/java/com/danrod/backend/franchiseinventory/FranchiseInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.danrod.backend.franchiseinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranchiseInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranchiseInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danrod/backend/franchiseinventory/FranchiseInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danrod.backend.franchiseinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchiseInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
